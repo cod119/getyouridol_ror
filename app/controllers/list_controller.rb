6017793f386ae9361973a865257532ab07189092
KDW <you@example.com>
2016-03-13 21:51:31 +0900

    initialize age-select

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -4,18 +4,29 @@ class ListController < ApplicationController
   end
   
   def search
-     @productionorunit = Idol.select(:productionorunit).distinct
+     @productionorunit = Idol.select(:productionorunit).distinct.order(productionorunit: :asc)
      @idol = Idol.new
-     @idols = Idol.all
+     @idols = Idol.all.order(nameko: :asc)
   end
   
   def result
-    @productionorunit = Idol.select(:productionorunit).distinct
-    @idols = Idol.all
+    @productionorunit = Idol.select(:productionorunit).distinct.order(productionorunit: :asc)
+    @idols = Idol.all.order(nameko: :asc)
     
     #성별
     if params[:gender] != ""
       @idols = @idols.where('gender = ?', params[:gender])
+    end
+    
+    ##{'6-8세'=>'6-8','9-11세'=>'9-11','12-14세'=>'12-14','15-17세'=>'15-17','18-19세'=>'18-19','20-24세'=>'20-24','25-29세'=>'25-29','30-34세'=>'30-34','35-40세'=>'35-40'}
+    
+    #연령
+    @ageMMarray = params[:age].split('-')
+    if params[:age] != "" and params[:age] != '5-41'
+      @idols = @idols.where('age >= ? AND age <= ?', @ageMMarray[0], @ageMMarray[1])
+    elsif params[:age] == '5-41'
+      @idols = @idols.where('age <= ? OR age >= ?', @ageMMarray[0], @ageMMarray[1])
+    else
     end
     
     #소속사
