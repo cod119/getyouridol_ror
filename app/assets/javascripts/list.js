78fdc46cbb0860782390710df02729bd2d5abc94
김동우 <idforcoding@gmail.com>
2016-03-16 20:41:55 +0000

    modify table link in search.html /and/ modify resposible action

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,28 +1,7 @@
 
 $(document).ready(function() {
 	
-	function once(func) {
-    // TIP: These variables are stored in a "closure scope" (worth researching),
-    // so that they'll remain available to the newly-generated function every
-    // time it's called.
-    	var alreadyCalled = false;
-    	var result;
 
-    // TIP: We'll return a new function that delegates to the old one, but only
-    // if it hasn't been called before.
-    	return function() {
-     	 if (!alreadyCalled) {
-        // TIP: .apply(this, arguments) is the standard way to pass on all of the
-        // infromation from one function call to another.
-      	  result = func.apply(this, arguments);
-      	  alreadyCalled = true;
-      	}
-      // The new function always returns the originally computed result.
-     	 return result;
-    	};
-  	};
-  	
-    	
   	
 	$('#search_form').on('change', '.select_min', function(e){
 		e.stopPropagation;
